02bacd683fad401d6915425843ed99896eea2500
manoj <manoj@Admins-MacBook-Air.local>
2018-02-18 14:53:18 +0530

    commit

--- rev_eng/settings.py
+++ rev_eng/settings.py
@@ -119,10 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #print(STATIC_ROOT)
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static'),
+    #os.path.join(BASE_DIR,'static'),
 ]
 
 
